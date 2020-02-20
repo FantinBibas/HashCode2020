f4341af99e92288cf6597f7c9e94309333ca696c
faudil.puttilli <faudil.puttilli@epitech.eu>
2020-02-20 18:56:29 +0100

    function parse_file created

--- main.py
+++ main.py
@@ -3,6 +3,17 @@
 import sys
 
 
+def parse_file(lines):
+    books_nbr, libraries_nbr, days = map(int, next(lines).split())
+    books = map(int, next(lines).split())
+    libraries = []
+    for line in lines:
+        library_param = map(int, line.split())
+        library_books = map(int, next(lines).split())
+        libraries.append({"param": library_param, "books": library_books})
+    return books_nbr, libraries_nbr, days, libraries
+
+
 def main(argv):
     if len(argv) != 2:
         return 1
