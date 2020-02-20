5b339a13e9b05475bddde1ca56971bc714d7286c
faudil.puttilli <faudil.puttilli@epitech.eu>
2020-02-20 19:03:35 +0100

    plop

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ def algo(books, days, librairies):
 
 
 def parse_file(lines):
-    books_nbr, libraries_nbr, days = map(int, next(lines).split())
-    books = map(int, next(lines).split())
+    books_nbr, libraries_nbr, days = map(int, lines[0].split())
+    books = map(int, lines[1].split())
     libraries = []
-    for line in lines:
-        library_param = map(int, line.split())
-        library_books = map(int, next(lines).split())
+    lines = lines[2:]
+    l = len(lines)
+    for i in range(0, l, 2):
+        library_param = map(int, lines[i].split())
+        library_books = map(int, lines[i + 1].split())
         libraries.append({"param": library_param, "books": library_books})
     return books_nbr, books, libraries_nbr, days, libraries
 
